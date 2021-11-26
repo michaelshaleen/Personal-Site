96efeca6e94f235ba90d03dc602dcf7a2c2e5c03
michaelshaleen <shaleenmichael@yahoo.com>
2021-11-25 18:43:18 -0600

    working ternary diplay to switch between paragraph and video

--- src/Components/Nutrition/Nutriton.jsx
+++ src/Components/Nutrition/Nutriton.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Nutrition.css';
 import Aos from 'aos';
 import "aos/dist/aos.css";
 import Button from '../Button/Button';
 
 
-function Nutrition() {
+function Nutrition() {  
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="nutrition-container">
-          <div 
+          <div
+            onClick={handleClick} 
             data-aos="flip-up"
             className="boxes">
             Nutrition
-          </div> 
-
-
-      <p>
+          </div>
+          
+      <p 
+          className={click ? 'content-show' : 'content-hide'}
+          onClick={handleClick} >
+          <div className="hi">
+            hi
+          </div>
           We've heard it before, it's why you're here reading me.
           Abs are made in the kitchen. You can't out 
           train a bad diet. Smoothie this, radical cleanse
@@ -25,7 +32,6 @@ function Nutrition() {
           necessarily <span className="easy">easy. </span>
           Bottom line is we have coached Performance Athletes
           , soccer moms, Senior Citizens, 
-
           <p>Currently we're operating on an individual
             basis(meaining you get your own coach) so
             no templates or apps at the moment!
